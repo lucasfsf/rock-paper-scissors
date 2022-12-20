34b0b164b64478014df472b2cfece5f089be0b10
lucasfsf <lucasfreireri@gmail.com>
2022-12-20 07:59:59 -0300

    Add getWinner functions

--- script.js
+++ script.js
@@ -29,6 +29,29 @@ function playRound(computerSelection, playerSelection) {
     }
     return message;
 }
-let computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice();
-console.log(playRound(computerSelection, playerSelection));
+
+function getWinner(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        console.log(`You win. Player Score = ${playerScore} > Computer Score = ${computerScore}`);
+    } else if (computerScore > playerScore) {
+        console.log(`You lose. Computer Score = ${computerScore} > Plahyer Score = ${playerScore}`);
+    } else {
+        console.log('It was a tie!')
+    }
+}
+
+function playGame() {
+    let rounds = 5;
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < rounds; i++) {
+        console.log(`Rounds ${i+1}!`);
+        console.log(`Player Score: ${playerScore}! | Computer Score: ${computerScore}`);
+        // Plays round
+        let computerSelection = getComputerChoice();
+        let playerSelection = getPlayerChoice();
+        console.log(playRound(computerSelection, playerSelection));
+    }
+    getWinner(playerScore, computerScore);
+}
+
