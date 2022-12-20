9c13e0226079eed6fed37814beacd8c865f20235
lucasfsf <lucasfreireri@gmail.com>
2022-12-20 07:40:41 -0300

    Add playRound function
    The playRound function takes two parameters, computerSelection and
    playerSelection and returns a message to display the winner of that
    round.

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+const CHOICES = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
-    let randomPick = Math.floor(Math.random() * choices.length);
-    return choices[randomPick];
+    let randomPick = Math.floor(Math.random() * CHOICES.length);
+    return CHOICES[randomPick];
 }
 
 function getPlayerChoice() {
     let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
     return playerChoice;
 }
+
+function playRound(computerSelection, playerSelection) {
+    let message;
+    if (computerSelection === playerSelection) {
+        message = `Its a tie. ${computerSelection} = ${playerSelection}`;
+    } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        message = "You lose. Rock beats Scissors.";
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        message = "You lose. Scissors beats Paper.";
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        message = "You lose. Paper beats Rock.";
+    } else {
+        message = `You win. ${playerSelection} beats ${computerSelection}`
+    }
+    return message;
+}
+let computerSelection = getComputerChoice();
+let playerSelection = getPlayerChoice();
+console.log(playRound(computerSelection, playerSelection));
